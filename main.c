5a5bcce3d9ecc6cfb6dd25b10310705db6737c53
medazza <midazza27@gmail.com>
2023-09-20 14:42:29 -0700

    change main

--- main.c
+++ main.c
@@ -10,36 +10,43 @@
 
 int main(int argc, char **argv, char **env)
 {
-	char *buffer = NULL;
+	char *buffer = NULL, **args;
 	size_t buffer_size = 0;
-	int n_char = 0, ind;
-	bool shell = false;
+	int n_char = 0;
 	(void)argc;
 
-	while (1 && !shell)
+	while (1)
 	{
-		if (isatty(STDIN_FILENO) == 0)
-			shell = true;
 		write(STDOUT_FILENO, "$ ", 2);
 		n_char = _getline(&buffer, &buffer_size, STDIN_FILENO);
-		if (n_char == EOF)
+		if (n_char == -1 || (n_char == 0 && isatty(STDIN_FILENO)))
 		{
 			if (isatty(STDIN_FILENO))
 				write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
-		ind = 0;
-		while (buffer[ind])
-		{
-			if (buffer[ind] == '\n')
-				buffer[ind] = '\0';
-			ind++;
-		}
-		if (buffer[0] == '\n')
+		if (buffer[n_char - 1] == '\n')
+			buffer[n_char - 1] = '\0';
+		if (buffer[0] == '\0')
 			continue;
-		handle_input(buffer, env, argv);
-		buffer_size = 0;
-		buffer = NULL;
+		if (strcmp(buffer, "exit") == 0)
+		{
+			free(buffer);
+			break;
+		}
+		args = tokenizez(buffer, " \t\r\n\a");
+		if (!check_cmd(args[0]))
+		{
+			write(STDOUT_FILENO, "Command not found: ", 19);
+			write(STDOUT_FILENO, args[0], _strlen(args[0]));
+			write(STDOUT_FILENO, "\n", 1);
+		}
+		else
+		{
+			run_cmd(args, env, argv);
+		}
+		free(args);
 	}
+	free(buffer);
 	return (0);
 }
